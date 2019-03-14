288233888e1f5b4e0035f46532ca7b20f37323ce
Jayesh Sharma <44506201+JAYESHSHARMA1@users.noreply.github.com>
2019-03-14 12:28:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,10 +20,7 @@ var express     = require("express"),
  var url = process.env.DATABASEURL || dbURL
  mongoose.connect(url);
  
-// mongoose.connect();
-// mongodb+srv://jay123:<password>@yccluster-oyz3a.mongodb.net/test?retryWrites=true
-// mongoose.connect("mongodb+srv://jay123:jayesh1234@yccluster-oyz3a.mongodb.net/test?retryWrites=true", { useNewUrlParser: true });
-// mongoose.connect("mongodb://localhost:27017/yelp_camp_v3", { useNewUrlParser: true });
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
